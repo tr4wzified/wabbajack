96ebf5ef2412ea720776986fc1590727510b000b
trawzified <55751269+tr4wzified@users.noreply.github.com>
2023-10-28 17:13:46 +0200

    Add WpfUI project, experimenting

---
+++ Wabbajack.App.WpfUI/Views/Pages/HomePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using Wabbajack.App.WpfUI.ViewModels.Pages;
+using Wpf.Ui.Controls;
+
+namespace Wabbajack.App.WpfUI.Views.Pages
+{
+    public partial class DashboardPage : INavigableView<HomeVM>
+    {
+        public HomeVM ViewModel { get; }
+
+        public DashboardPage(HomeVM viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ Wabbajack.App.WpfUI/Views/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using Wabbajack.App.WpfUI.ViewModels.Pages;
+using Wpf.Ui.Controls;
+
+namespace Wabbajack.App.WpfUI.Views.Pages
+{
+    public partial class SettingsPage : INavigableView<SettingsVM>
+    {
+        public SettingsVM ViewModel { get; }
+
+        public SettingsPage(SettingsVM viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+    }
+}
