8dab84a2286d4eabee2f65947e1edb05aa9b8f56
trawzified <55751269+tr4wzified@users.noreply.github.com>
2023-10-28 17:39:46 +0200

    Create Avalonia project with FluentAvalonia

---
+++ Wabbajack.App.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Wabbajack.App.Avalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
